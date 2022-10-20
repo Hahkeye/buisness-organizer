dd88a327bdb370775515b1d7750ac558f5156d9f
Hunter <realisvenator@gmail.com>
2022-10-19 19:36:52 -0500

    All view sand adds and updats functiong plus some refactro

---
+++ helpers/dbhelper.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql2/promise');
+class Field{
+    constructor(blob){
+        this
+    }
+}
+
+
+class DBhelper{
+
+    constructor(consetup){
+        this.connection = mysql.createConnection(consetup);
+    }
+
+    async get(table){
+        let temp = await (await this.connection).query(`SELECT * from ${table}`);
+        return temp[0];//need to listify the dictionary and make the id and the name the first two things so you can display it
+    }
+    async insert(table,data,values){
+        console.log(table);
+        console.log(values);
+        values = Object.values(values).map(val=>`"${val}"`)
+        console.log(data);
+        console.log(`INSERT INTO ${table} (${data}) VALUES (${values});`);//add "?" before all string literals so mysql does nice things
+        let temp = await (await this.connection).execute(`INSERT INTO ${table} (${data}) VALUES (${values});`)
+    }
+
+    async edit(table,changeField,changeValue,target){
+        let temp = await(await this.connection).execute(`UPDATE ${table} set ${changeField} = ${changeValue} WHERE id = ${target}`);
+    }
+    async info(table){
+        let temp = await(await this.connection).query(`SHOW COLUMNS FROM ${table}`);
+        let t = temp[0].map(x => [x['Field'],x['Type'],x['Key']]);
+        return t;
+        
+    }
+
+    async tables(){
+        let temp = await(await this.connection).query('SHOW TABLES');
+        // console.log(temp[0]);
+        return temp[0].map(val => val['Tables_in_buisness']);
+    }
+
+    async search(){
+        let temp = await(await this.connection).query();
+    }
+
+}
+
+module.exports = DBhelper;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
+const { info } = require('console');
+const e = require('express');
 const inquirer = require('inquirer');
+const DBhelper = require('./helpers/dbhelper');
 
-const connection = mysql.createConnection({
+const connection = new DBhelper({
     host: 'localhost',
     user: 'root',
     password: 'root',
@@ -19,16 +22,13 @@ function menu(){
     4. Add department
     5. Add role
     6. Add employee
-    7. Update employee role.
+    7. Update entry.
+    9. view employees by group
+    10. delete something
+    11. search something
     `);
 }
 
-async function getAll(table){
-    let con = await connection;
-    let t = await con.query(`SELECT * from ${table}`);
-    // console.log(t[0]);
-    return t[0];
-}
 async function addEmployee(){
     let temp = [];
     for(let i of await getAll('buisness.role')){
@@ -67,14 +67,14 @@ async function addDepartment(){
             type: 'input',
             name: 'name',
             message: 'What is the Department name?'
-        },
+        }
 
     ]);
 }
 
 async function addRole(){
     let temp = [];
-    let t = getAll("buisness.department");
+    let t = await connection.get("buisness.department");
     for(let i of t){
         temp.push({name: i['name'],value: i['id']});
     }
@@ -99,55 +99,90 @@ async function addRole(){
 }
 
 
-
-async function getDepartments(){ //mayb refactor these getters to just query the db with the string and not have 3 fucntions
-    let t = await getAll('buisness.department');
-    if(t){
-        return t;
-    }
-}
-
-async function getEmployees(){
-    let t = await getAll('buisness.employee');
-    if(t){
-        return t;
-    }
-}
-
-async function getRoles(){
-    let t = await getAll('buisness.role')
-    if(t){
-        return t;
-    }
-}
-
-async function editEmployee(){
-    let temp2 = [];
-    for(let i of await getAll('buisness.role')){
-        console.log(i['title']);
-        temp2.push({name: i['title'],value: i['id']});
-    }
-    let temp=[];
-    let t = await getEmployees();
-    console.log(t);
-    for(let i of t){
-        temp.push({name: i['first_name'],value: i['id']});
-    }
-    return await inquirer.prompt([
+async function edit(){
+    let tables = await connection.tables();
+    let sel1= await inquirer.prompt(
         {
             type: 'list',
-            name: 'employee',
-            message: 'What employee would you like to edit?',
-            choices: temp
-        },
-        {
-            type: 'list',
-            name: 'title',
-            message: 'What role would you like to change to?',
-            choices: temp2
+            name: 'table',
+            message: 'What would you like to edit?',
+            choices: tables
         }
-    ]); 
+    );
+    let records = await connection.get(sel1.table);//all values in the table
+    // console.log(sel1);//target table
+    let targets = [];
+    let keyl = null;
+    for(let i in records){
+        console.log();
+        let keys = Object.keys(records[i]);
+        let id = records[i]['id'];
+        keys.shift();
+        keyl=keys;
+        let temp = "";
+        keys.forEach(x => temp+=` ${x}: ${records[i][x]} |`);
+        targets.push({name: temp,value: id});
+    }
+    // console.log(targets);
+    let sel2 = await inquirer.prompt(
+        {
+            type: 'list',
+            name: 'target',
+            message: 'Which one would you like to edit?',
+            choices: targets
+        }
+    );
+    // console.log(sel2);
+    let sel3 = await inquirer.prompt(
+        {
+            type: 'list',
+            name: 'field',
+            message: 'Which one would you like to edit?',//Need to ask about params
+            choices: keyl  
+        }
+
+    );
+    if(tables.includes(sel3.field.split("_")[0])){
+        let valus = await connection.get(sel3.field.split("_")[0]);
+        let targets2=[];
+        for(let i in valus){
+            console.log();
+            let keys = Object.keys(valus[i]);
+            let id = valus[i]['id'];
+            keys.shift();
+            keyl=keys;
+            let temp = "";
+            keys.forEach(x => temp+=` ${x}: ${valus[i][x]} |`);
+            targets2.push({name: temp,value: id});
+        }
+        // console.log(valus);
+        let sel4 = await inquirer.prompt(
+            {
+                type: 'list',
+                name: 'value',
+                message: 'Which one would you like to edit?',//Need to ask about params
+                choices: targets2  
+            }
+    
+        ); 
+        // console.log(sel4);
+        await connection.edit(sel1.table,sel3.field,sel4.value,sel2.target);
+    }else{
+        // console.log("Let user input");
+        let sel4 = await inquirer.prompt(
+            {
+                type: 'input',
+                name: 'value',
+                message: 'Which one would you like change to?',//Need to ask about params
+            }
+    
+        ); 
+        console.log(sel4);
+        await connection.edit(sel1.table,sel3.field,`"${sel4.value}"`,sel2.target);
+        
+    }
 }
+
 
 async function main(){
     let sel=null
@@ -167,22 +202,20 @@ async function main(){
         // console.log(sel);
         switch(sel['selection']){
             case 1:
-                let deps = await getDepartments();
-                // console.log(deps);
-                console.log(`Department Name|Department Number`);
-                for(let i of deps){
-                    console.log(`${i.name}|${i.id}`);
-                }
+                let dat = await connection.get('buisness.department')
+                console.log(dat);
+                connection.info('buisness.department');
                 break;
             case 2:
-                let roles = await getRoles();
+                let roles = await connection.get('buisness.role');
+                connection.info('buisness.role');
                 console.log(`RoleID|Role Title|Role Salary|Role department ID`);
                 for(let i of roles){
                     console.log(`ID: ${i.id} | Title: ${i.title} |  Salary: ${i.salary} | DepId: ${i.department_id}`);
                 }
                 break;
             case 3:
-                let emp = await getEmployees();
+                let emp = await connection.get('buisness.employee');
                 console.log(`Employe Name|Employee LastName|Role|Manager`);
                 for(let i of emp){
                     console.log(`Name: ${i.first_name}|Last Name: ${i.last_name}| RoleID ${i.role_id}| MangerID ${i.manager_id} | ID ${i.id}`);
@@ -191,28 +224,25 @@ async function main(){
             case 4:
                 let temp2 =  await addDepartment();
                 console.log(temp2);
-                let con5 = await connection;
-                let t6 = await con5.execute(`INSERT INTO buisness.department (name) VALUES ('${temp2.name}');`);
+                connection.insert('buisness.department','name',[temp2.name]);
                 break;
             case 5:
                 let temp3 = await addRole();
                 console.log(temp3);
-                let con2 = await connection;
-                let t2 = await con2.execute(`INSERT INTO buisness.role (title, salary, department_id) VALUES ('${temp3.title}','${temp3.salary}','${temp3.department}');`);
+                connection.insert('buisness.role',['title','salary','department_id'],[temp3.title,temp3.salary,temp3.department]);
                 break;
             case 6:
                 let temp = await addEmployee();
                 console.log(temp);
-                let con = await connection;
-                let t = await con.execute(`INSERT INTO buisness.employee (first_name, last_name, role_id, manager_id) VALUES ('${temp.name}','${temp.lname}','${temp.roleID}','${temp.manager}');`);
+                connection.insert('buisness.employee',['first_name','last_name','role_id','manager_id'],[temp.name,temp.lname,temp.roleID,temp.manager]);
                 // console.log(t);
                 break;
             case 7:
-                let a = await editEmployee();
+                let a = await edit();
                 console.log(a);
-                let con6 = await connection;
-                let asd = await con6.execute(`UPDATE buisness.employee SET role_id = ${a.title} WHERE id = ${a.employee};`);
-                
+                break;
+            case 8:
+
                 break;
         }
     }
